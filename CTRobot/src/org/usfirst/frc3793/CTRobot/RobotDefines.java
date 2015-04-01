7f2eef5dbc5de88d2be1a9d05bb7c57197f8414c
saturn28 <jeffcroc28@gmail.com>
2015-04-01 14:41:19 -0400

    Prepare the code with the structure and PIDs to make it competition ready.  Still having trouble with teleop accessing the command moverobot(double speed).

---
+++ CTRobot/src/org/usfirst/frc3793/CTRobot/RobotDefines.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc3793.CTRobot;
+
+public class RobotDefines {
+	public static boolean simulation = true;
+	public static boolean debug = true;
+	public static double  cEncPulseDist = 0.0349;
+	public static double  cEncPulseDistSim = 0.042; //TODO: Needs to be changed
+}
